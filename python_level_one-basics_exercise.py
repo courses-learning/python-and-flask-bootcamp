1876bc0950fb43002ea6b61ec71fc5197d555848
David Weatherspoon <david_weatherspoon@hotmail.com>
2019-07-06 09:17:13 +0100

    Very simple exercises ticked off for initial python section.

---
+++ python_level_one-basics_exercise.py
@@ -0,0 +1,71 @@
+#####################################
+#### PART 6: EXERCISE REVIEW  #######
+#####################################
+
+# Time to review all the basic data types we learned! This should be a
+# relatively straight-forward and quick assignment.
+
+###############
+## Problem 1 ##
+###############
+
+# Given the string:
+s = 'flask'
+
+# Use indexing to print out the following:
+# 'f'
+print(s[0])
+# 's'
+print(s[3])
+# 'ask'
+print(s[2:])
+# 'las'
+print(s[1:4])
+# 'k'
+print(s[4])
+# Bonus: Use indexing to reverse the string
+print(s[::-1])
+
+###############
+## Problem 2 ##
+###############
+
+# Given this nested list:
+mylist = [3, 7, [1, 4, 'hello']]
+# Reassign "hello" to be "goodbye"
+mylist[2][2] = 'goodbye'
+print(mylist)
+
+###############
+## Problem 3 ##
+###############
+
+# Using keys and indexing, grab the 'hello' from the following dictionaries:
+
+d1 = {'simple_key': 'hello'}
+print(d1['simple_key'])
+
+d2 = {'k1': {'k2': 'hello'}}
+print(d2['k1']['k2'])
+
+d3 = {'k1': [{'nest_key': ['this is deep', ['hello']]}]}
+print(d3['k1'][0]['nest_key'][1][0])
+
+###############
+## Problem 4 ##
+###############
+
+# Use a set to find the unique values of the list below:
+mylist = [1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3]
+print(set(mylist))
+
+###############
+## Problem 5 ##
+###############
+
+# You are given two variables:
+age = 4
+name = "Sammy"
+
+# Use print formatting to print the following string:
+print(f"Hello my dog's name is {name} and he is {age} years old")
