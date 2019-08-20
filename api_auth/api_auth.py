d7b95ba2b1133e541d2808b45084a17b36b02fd7
David Weatherspoon <david_weatherspoon@hotmail.com>
2019-08-20 20:43:49 +0100

    added database functionality to api example

--- api_auth/api_auth.py
+++ api_auth/api_auth.py
@@ -1,36 +1,54 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
 from secure_check import authenticate, identity
 from flask_jwt import JWT, jwt_required
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
+
 app.config['SECRET_KEY'] = 'mysecretkey'
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+Migrate(app, db)
 
 api = Api(app)
 jwt = JWT(app, authenticate, identity)
 
-people = []
+
+class Person(db.Model):
+    name = db.Column(db.String(80), primary_key=True)
+
+    def __init__(self, name):
+        self.name = name
+
+    def json(self):
+        return {'name': self.name}
 
 
 class Name(Resource):
 
     def get(self, name):
-        for person in people:
-            if person['name'] == name:
-                return person
-
-        return {'name': None}, 404
+        person = Person.query.filter_by(name=name).first()
+        if person:
+            return person.json()
+        else:
+            return {'name': None}, 404
 
     def post(self, name):
-        person = {'name': name}
-        people.append(person)
-        return person
+        person = Person(name=name)
+        db.session.add(person)
+        db.session.commit()
+        return person.json()
 
     def delete(self, name):
-        for ind, person in enumerate(people):
-            if person['name'] == name:
-                deleted_person = people.pop(ind)
-                return {'note': 'delete success'}
+        person = Person.query.filter_by(name=name).first()
+        db.session.delete(person)
+        db.session.commit()
+        return {'note': 'delete success'}
 
 
 class AllNames(Resource):
@@ -38,7 +56,9 @@ class AllNames(Resource):
     # Added decorator menans fuction requires authentication to use
     @jwt_required()
     def get(self):
-        return {'people': people}
+        people = Person.query.all()
+
+        return [person.json() for person in people]
 
 
 api.add_resource(Name, '/person/<string:name>')
